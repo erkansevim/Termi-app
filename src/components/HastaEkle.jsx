33cadb2718167870944c01d2038d64a2ff449a45
erkansevim <erkansevim.essgmail.com>
2024-07-18 09:23:47 +0200

    first commit

---
+++ src/components/HastaEkle.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import uuid from "react-uuid"
+const HastaEkle = ({hastalar, setHastalar,doktorlar}) => {
+const[hastaName,setHastaName]=useState("")
+const[hastaTarih,setTarih]=useState("")
+
+console.log(doktorlar);
+
+const handleSubmitt=(e)=>{
+  e.preventDefault(); // direk submit olayını yapma önce alttaki kodlara bak
+
+  setHastalar([
+    ...hastalar,
+    {
+      id: uuid(),
+      text: hastaName,
+      day: hastaTarih,
+      isDone: false,
+      myDoctor: doktorlar[0].doctorName,
+    },
+  ]);
+
+  // setHastalar a gidildiğinde hemen altta console.log varsa çalışıverir ve biz useState nin yaptığı işi göremeyiz. bunun için log bu submit fonksiyonunun dışında olmalı.
+
+  setHastaName("");
+  setTarih("");
+  // submit sonrası inputlardan value temizlemek için, hem üstteki işlemler yapılır, hemde inputlarda value={isim} yazarak browser da boşluksa boşluk isimse isim gözükmesi sağlanır
+}
+console.log(hastalar);
+  return (
+    <div>
+      <form onSubmit={handleSubmitt}>
+        <div className="formControl">
+          <label htmlFor="text">Hasta Bilgileri</label>
+          <input
+            type="text"
+            id="text"
+            onChange={(e) => setHastaName(e.target.value)}
+
+            value={hastaName}
+          />
+        </div>
+
+        <div className="formControl">
+          <label htmlFor="day">Day & Time</label>
+          <input
+            type="datetime-local"
+            id="day"
+            onChange={(e) => setTarih(e.target.value)}
+            value={hastaTarih}
+          />
+        </div>
+
+        <button type="submit" className="kayit btn-submit">
+          <span style={{ color: "#6a0707" }}> {doktorlar[0].doctorName}</span> için kayıt
+          oluştur
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default HastaEkle
---
+++ src/components/HastaListe.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { FaTimesCircle } from "react-icons/fa";
+const HastaListe = ({ hastalar, setHastalar, doktorlar }) => {
+ 
+  // if(doktorlar.length===1){
+  //   setHastalar(hastalar.filter((a)=> a.myDoctor===doktorlar[0].doctorName))
+  // }else setHastalar(hastalar)
+  // üstteki gibi yazarsam doktorlar dizisi teke düştüğünde sonsuz döngü onblur, çünkü 1 doktor varken setHastalar a git diyoruz, gidip buraya döndüğünde doktor sayısı 1 old için yine set çalışır
+
+  // başka yol
+  // doktorlar.length === 4 ? (ilk yaptığımız gibi hastalarda gez hepsini bastır) : 
+
+  // doktorlar[0].doktor === doktorum && (soldaki şarta uyanları bastır)
+
+  //son yol da alttaki doktorlar 4 olsa da bir olsa da ismi geçen doktorun hastalarını bastır
+  // hastalarda gezerken ilk hastayı al, bişey bastırmadan doktorlarda gezin, elindeki hastanın doktoru,o anki doktorlar dizisinde varsa, o hastayı bastır. 4 doktor varken bütün hastalar basılır. tek doktor varken, bahsi geçen doktorun hastaları bastırılır
+  
+  return (
+    <div >
+     
+      {hastalar.map((hasta) => (
+        <div key={hasta.id}>
+         (
+                <div
+                 
+                  className={hasta.isDone ? "trueStil" : "falseStyle"}
+                  onDoubleClick={() =>
+                    setHastalar(
+                      hastalar.map((a) =>
+                        a.id === hasta.id
+                          ? { ...a, isDone: !a.isDone }
+                          : a
+                      )
+                    )
+                  }
+                >
+                  <div>
+                    <h2>{hasta.text} </h2>
+                    <h4>{hasta.day} </h4>
+                    <h3>{hasta.myDoctor} </h3>
+                  </div>
+                  <div>
+                    <FaTimesCircle
+                      style={{ color: "red" }}
+                      onClick={() =>
+                        setHastalar(hastalar.filter((a) => a.id !== hasta.id))
+                      }
+                    />
+                  </div>
+                </div>
+              )
+        
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default HastaListe;
